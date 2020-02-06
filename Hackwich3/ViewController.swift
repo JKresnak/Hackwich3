67c2381f22113be9e2f9bc4000ee34e5f4994e58
Jkresnak <cmstudent@uhwo-acm-01.uhwo.hawaii.edu>
2020-02-06 10:32:38 -1000

    Hackwich3 parts 1-3 2/6/2020

--- Hackwich3/ViewController.swift
+++ Hackwich3/ViewController.swift
@@ -14,7 +14,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    var firstString = "The background color will turn red"
+    var secondString = "Turns blue"
+   
+    @IBAction func ChangecoloronButtonPress(_ sender: Any) {
+        if firstString == "The background color will turn blue"
+    {
+            self.view.backgroundColor = UIColor.red
+    }
+    else
+    {
+        self.view.backgroundColor = UIColor.blue
+    }
+    
+    }
 
 }
+
+
 
